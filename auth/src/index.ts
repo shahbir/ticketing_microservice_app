02d580bc3da90bcce30a3983625df3a4b42fcb20
Danish Shafiq <danishshafiq030@gmail.com>
2022-10-25 19:29:26 +0500

    skaffold done, ingress nginx continue

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,6 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-app.listen(5000, () => {
-  console.log('Listening on port 5000!');
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
 });
