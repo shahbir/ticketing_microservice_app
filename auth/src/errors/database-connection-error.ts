c05b86c6b3adf35cc021112195fcd2dba029f903
Danish Shafiq <danishshafiq030@gmail.com>
2022-11-10 12:49:17 +0500

    setup in process for proper set the all error responses

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,9 @@
+export class DatabaseConnectionError extends Error {
+  reason = 'Error Connecting to database';
+
+  constructor() {
+    super();
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+}
--- auth/src/routes/userRoutes.ts
+++ auth/src/routes/userRoutes.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const route = express.Router();
 
@@ -17,8 +19,10 @@ route.post(
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      throw new Error('Invalid email or password');
+      throw new RequestValidationError(errors.array());
     }
+
+    throw new DatabaseConnectionError();
 
     res.send('hello from signup');
   }
